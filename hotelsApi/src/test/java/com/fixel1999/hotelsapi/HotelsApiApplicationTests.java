d4448065a4d7af0d0834b584a88259f02e7eee77
fixel1999 <fixel1999@gmail.com>
2025-10-16 22:07:00 +0100

    Initial commit

---
+++ hotelsApi/src/test/java/com/fixel1999/hotelsapi/HotelsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fixel1999.hotelsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
