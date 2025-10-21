7eae53df4fc79638851ab88b4c11d6894919a041
fixel1999 <fixel1999@gmail.com>
2025-10-21 01:37:38 +0100

    Revert "Updated loading page"
    This reverts commit d5b0d48063c96383594c15ac293fabed445f48ae.

--- react-client/src/components/loading.tsx
+++ react-client/src/components/loading.tsx
@@ -2,16 +2,10 @@
 import React from "react";
 import {
 	Flex,
-	Heading,
-	Highlight,
 	Spinner,
 } from "@chakra-ui/react";
 import { useLoading } from "@/context/loadingContext";
-import { Saira } from "next/font/google";
 
-const saira = Saira({
-	subsets: ['latin']
-})
 
 export default function GlobalLoading() {
 	const { isLoading } = useLoading();
@@ -24,17 +18,13 @@ export default function GlobalLoading() {
 			w="100vw"
 			h="100vh"
 			zIndex={9999}
-			bg={"#ceffde"}
+			bg={"#ceffdea6"}
 			backdropFilter="blur(5px)"
 			align="center"
 			justify="center"
 			direction="column"
 			color={"green.500"}
-			gap={5}
 		>
-			<Heading className={saira.className} size={"5xl"} cursor={"pointer"} color={"#031621"}>
-				<Highlight query="Hotels" styles={{ color: "green.500" }}>Hotels Management</Highlight>
-			</Heading>
 			<Spinner
 				size="xl"
 				borderWidth={'4px'}
